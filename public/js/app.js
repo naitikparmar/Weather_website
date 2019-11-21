95c3cbde00e6faf5658e455ecc4c551360a928f2
unknown <parmar.naitik1@gmail.com>
2019-11-21 20:06:18 +0530

    init Git

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('Client side javascript file is loaded!')
+
+
+
+const weatherForm = document.querySelector('form')
+const searchinput =document.querySelector('input')
+const msg1 =document.querySelector('#msg1')
+const msg2 = document.querySelector('#msg2')
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location = searchinput.value
+    
+   msg2.textContent ='Loading...'
+   
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+     response.json().then((data)=>{
+         if(data.error){
+             msg1.textContent= data.error
+         }
+         else{
+            msg1.textContent = ''+data.location
+            msg2.textContent =data.forecast
+         }
+    
+    console.log(data.address)
+    console.log(data.location)
+    console.log(data.forecast)
+})
+    
+})
+
+})
